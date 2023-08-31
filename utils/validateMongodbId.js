acca58f6e664bf0484072f83f0e83071bbbf3ca6
matin <matinsadegi3290@gmail.com>
2023-08-31 21:57:08 +0330

    create validateMongodbId for check mongo id

---
+++ utils/validateMongodbId.js
@@ -0,0 +1,5 @@
+import mongoose from "mongoose";
+export const validateMongoDbId = (id) => {
+    const isValid = mongoose.Types.ObjectId.isValid(id);
+    if(!isValid) throw new Error("This id is not valid or not found")
+}
