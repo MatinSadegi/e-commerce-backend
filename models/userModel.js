fecfb021e33db67e62e4b438af1968d0582ec0d3
matin <matinsadegi3290@gmail.com>
2023-09-01 11:17:50 +0330

    add some feature for user model

--- models/userModel.js
+++ models/userModel.js
@@ -6,11 +6,15 @@ const userSchema = mongoose.Schema(
     lastName: { type: String, required: true },
     email: { type: String, required: true },
     phoneNumber: { type: Number, required: true, unique: true },
-    password: { type: String, required: true},
+    password: { type: String, required: true },
+    role: { type: String, default: "user" },
+    cart: { type: Array, default: [] },
+    address: { type: String },
+    wishlist: [{ type: mongoose.Schema.Types.ObjectId, ref: "Product" }],
+    refreshToken:{type:String}
   },
   { timestamps: true }
 );
 
-
-const User = mongoose.model("User",userSchema);
-export default User
+const User = mongoose.model("User", userSchema);
+export default User;
