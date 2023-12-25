26e21d96af4ac731b5131f505c16c9a6d0c79b4c
matin <matinsadegi3290@gmail.com>
2023-12-26 01:08:45 +0330

    fix user model

--- models/userModel.js
+++ models/userModel.js
@@ -2,16 +2,13 @@ import mongoose from "mongoose";
 
 const userSchema = new mongoose.Schema(
   {
-    firstName: { type: String, required: true },
-    lastName: { type: String, required: true },
+    name: { type: String, required: true },
     email: { type: String, required: true },
-    phoneNumber: { type: Number, required: true, unique: true },
     password: { type: String, required: true },
-    role: { type: String, default: "user" },
+    role: { type: String, default: "USER" },
     cart: { type: Array, default: [] },
-    address: { type: String },
     wishlist: [{ type: mongoose.Schema.Types.ObjectId, ref: "Product" }],
-    refreshToken:{type:String}
+    refreshToken: { type: String },
   },
   { timestamps: true }
 );
