01058f7ef2a6170fecb3f2e6115eaebffc961e11
matin <matinsadegi3290@gmail.com>
2023-12-26 19:57:21 +0330

    fix refreshTokenHandler

--- controller/refreshTokenController.js
+++ controller/refreshTokenController.js
@@ -1,6 +1,6 @@
 import asyncHandler from "express-async-handler";
 import User from "../models/userModel.js";
-import jwt, { decode } from "jsonwebtoken";
+import jwt from "jsonwebtoken";
 import { generateToken } from "../utils/generateToken.js";
 import { generateRefreshToken } from "../utils/refreshToken.js";
 export const handleRefreshToken = asyncHandler(async (req, res) => {
@@ -12,15 +12,34 @@ export const handleRefreshToken = asyncHandler(async (req, res) => {
     secure: true,
     sameSite: "none",
   });
-  const foundUser = await User.findOne({ refreshToken }).exec();
+  const foundUser = await User.findOne({ refreshToken });
   //Detected refresh token reuse!
   if (!foundUser) return res.sendStatus(403); //Forbidden
-  jwt.verify(refreshToken, process.env.ACCESS_TOKEN_SECRET, (err, decode) => {
-    if ((err, foundUser.id !== decode.id)) return res.sendStatus(403);
-    const accessToken = generateToken(foundUser?._id);
-    // const newRefreshToken = generateRefreshToken(foundUser?._id);
-    // foundUser.refreshToken = newRefreshToken
-    // const result = await foundUser.save();
-    res.cookie("accessToken", accessToken, { maxAge: 60000 });
-  }); 
+  jwt.verify(
+    refreshToken,
+    process.env.ACCESS_TOKEN_SECRET,
+    async (err, decode) => {
+      if (err | (foundUser._id.toString() !== decode.id)) {
+        return res.sendStatus(403);
+      } else {
+        const accessToken = generateToken(foundUser?._id);
+        const newRefreshToken = generateRefreshToken(foundUser?._id);
+        foundUser.refreshToken = newRefreshToken;
+        await foundUser.save();
+        res.cookie("refreshToken", newRefreshToken, {
+          maxAge: 1000 * 60 * 60 * 24 * 7,
+          httpOnly: true,
+          sameSite: "Lax",
+          secure: true,
+        });
+        res.cookie("accessToken", accessToken, {
+          maxAge: 1000 * 60 * 60 * 24 * 7,
+          httpOnly: true,
+          sameSite: "Lax",
+          secure: true,
+        });
+        res.send("new accessToken was created");
+      }
+    }
+  );
 });
