cd74db4395c0553694265b3561723a97011cbef6
matin <matinsadegi3290@gmail.com>
2024-03-29 17:02:13 +0330

    add coupon

--- controller/couponController.js
+++ controller/couponController.js
@@ -12,6 +12,47 @@ export const createCoupon = asyncHandler(async (req, res) => {
   }
 });
 
-// const applyCoupon = asyncHandler(async (req, res) => {
-//   const { coupon } = req.body;
-// });
+export const getAllCoupons = asyncHandler(async (req, res) => {
+  try {
+    const coupons = await Coupon.find();
+    res.json(coupons);
+  } catch (error) {
+    throw new Error(error);
+  }
+});
+
+export const getCoupon = asyncHandler(async (req, res) => {
+  const { id } = req.params;
+  try {
+    const coupon = await Coupon.findById(id);
+    res.json(coupon);
+  } catch (error) {
+    throw new Error(error);
+  }
+});
+
+export const deleteCoupon = asyncHandler(async (req, res) => {
+  const { id } = req.params;
+  try {
+    const deleteCoupon = await Coupon.findByIdAndDelete(id);
+    res.json(deleteCoupon);
+  } catch (error) {
+    throw new Error(error);
+  }
+});
+
+export const applyCoupon = asyncHandler(async (req, res) => {
+  const { coupon } = req.body;
+  const _id = req.userId;
+  const validCoupon = await Coupon.findOne({ name: coupon });
+
+  if (validCoupon === null) {
+    throw new Error("Invalid Coupon");
+  }
+  let { cartTotal } = await Cart.findOne({ orderby: _id });
+  let discount = (cartTotal * validCoupon.discount) / 100;
+  await Cart.findOneAndUpdate({ orderby: _id }, { discount }, { new: true });
+  res.json({
+    discount,
+  });
+});
--- routes/couponRoutes.js
+++ routes/couponRoutes.js
@@ -1,8 +1,14 @@
 import express from "express";
-import { createCoupon } from "../controller/couponController.js";
+import { applyCoupon, createCoupon, deleteCoupon, getAllCoupons, getCoupon} from "../controller/couponController.js";
+import { protect } from "../middlewares/authMiddleware.js";
 
 const router = express.Router();
 
 router.post("/", createCoupon);
+router.get("/", getAllCoupons);
+router.get("/:id", getCoupon);
+router.delete('/:id',deleteCoupon)
+router.post('/apply-coupon',protect,applyCoupon)
 
 export default router;
+ 
